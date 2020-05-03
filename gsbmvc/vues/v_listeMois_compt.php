59920ff9aeb42b3abd21af5b154e8b5f025e0930
W0raX <aronpabalan@gmail.com>
2020-05-03 21:32:16 +0200

    Add files via upload

---
+++ gsbmvc/vues/v_listeMois_compt.php
@@ -0,0 +1,27 @@
+ <div id="contenu">
+      <h2>validation des fiches de frais</h2>
+      <h3>Visiteur et mois à sélectionner : </h3>
+            <form action="index.php?uc=etatFrais&action=voirEtatFrais" method="post">
+      <div class="corpsForm">
+      <form action="index.php?uc=etatFrais&action=voirEtatFrais" method="post">
+      <div class="corpsForm">
+         
+      <p>
+	 
+        <label for="lstMois" accesskey="n">Mois : </label>
+        <select id="lstMois" name="lstMois">
+            <?php
+			foreach ($lesMois as $unMois)
+			{
+			    $mois = $unMois['mois'];
+				$numAnnee =  $unMois['numAnnee'];
+				$numMois =  $unMois['numMois'];
+				if($mois == $moisASelectionner){
+				?>
+				<option selected value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option>
+				<?php 
+				}
+				else{ ?>
+				<option value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option>
+				<?php 
+				}
